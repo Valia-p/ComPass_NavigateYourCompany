225dbb6cbe17fd33b941294580ae9a178137d64d
Panagiota Nalmpanti <nspanagi@csd.auth.gr>
2024-08-09 11:20:32 +0300

    first 2/2 commit

--- app/src/main/java/com/example/compass_navigateyourcompany/AppDatabase.java
+++ app/src/main/java/com/example/compass_navigateyourcompany/AppDatabase.java
@@ -2,7 +2,6 @@ package com.example.compass_navigateyourcompany;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-//Yota is PERFECT
 @Database(entities = {User.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao userDao();
